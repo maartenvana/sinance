9eacf4509112b574b86f9b0853fec31eea4df6f1
Maarten van Arem <maartenva@LIVE.COM>
2019-09-25 07:54:24 +0200

    Removed UserId field from SinanceUser

--- Sinance.Domain/Entities/SinanceUser.cs
+++ Sinance.Domain/Entities/SinanceUser.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sinance.Domain.Entities
+﻿namespace Sinance.Domain.Entities
 {
     public class SinanceUser : EntityBase
     {
         public string Password { get; set; }
-        public string UserId { get; set; }
+
         public string Username { get; set; }
     }
 }

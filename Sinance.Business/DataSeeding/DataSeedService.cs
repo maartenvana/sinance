086697b75d12b76cdb343897fc34b6eb4f94789c
Maarten van Arem <maartenva@LIVE.COM>
2019-10-13 11:54:44 +0200

    Improved data seed

--- Sinance.Business/DataSeeding/DataSeedService.cs
+++ Sinance.Business/DataSeeding/DataSeedService.cs
@@ -52,7 +52,7 @@ namespace Sinance.Business.DataSeeding
             var investmentAccount = InsertBankAccount(unitOfWork, user, "Investments", BankAccountType.Investment);
 
             await DeleteCategoriesAndTransactions(unitOfWork, user);
-            InsertCategoriesAndTransactions(unitOfWork, user, mainBankAccount, secondaryBankAccount, savingsAccount, investmentAccount);
+            InsertCategoriesAndTransactions(unitOfWork, user, mainBankAccount, savingsAccount);
 
             await unitOfWork.SaveAsync();
 
@@ -100,33 +100,52 @@ namespace Sinance.Business.DataSeeding
             return bankAccount;
         }
 
-        private void InsertCategoriesAndTransactions(IUnitOfWork unitOfWork, SinanceUser user, BankAccount primaryChecking, BankAccount secondaryChecking, BankAccount savings, BankAccount investments)
+        private void InsertCategoriesAndTransactions(IUnitOfWork unitOfWork, SinanceUser user, BankAccount primaryChecking, BankAccount savingsAccount)
         {
             _logger.Information("Creating demo categories and transactions");
 
-            var essentialsCategory = InsertCategory(unitOfWork, user, "Essentials", false);
+            // TODO: This should be a standard category, already created issue
+            //var essentialsCategory = InsertCategory(unitOfWork, user, "Essentials", false);
 
-            var foodCategory = InsertCategory(unitOfWork, user, "Food", false, essentialsCategory);
+            var mortgageCategory = InsertCategory(unitOfWork, user, "Mortgage", true);
+            InsertMonthlyTransactionsForCategory(unitOfWork, primaryChecking, mortgageCategory, 3, "Mortgage payment", "Bank", -1000, -1000);
+
+            var foodCategory = InsertCategory(unitOfWork, user, "Food", false);
             InsertWeeklyTransactionsForCategory(unitOfWork, primaryChecking, foodCategory, DayOfWeek.Saturday, "FoodMarket", "Groceries", -60, -40);
+            InsertRandomMonthlyTransactionsForCategory(unitOfWork, primaryChecking, foodCategory, "Dinner for 2", "Restaurant", -100, -75);
 
-            var salaryCategory = InsertCategory(unitOfWork, user, "Salary", true, essentialsCategory);
+            var salaryCategory = InsertCategory(unitOfWork, user, "Salary", true);
             InsertMonthlyTransactionsForCategory(unitOfWork, primaryChecking, salaryCategory, 25, "Salary", "Company", 2000, 2000);
 
-            var electricityAndGasCategory = InsertCategory(unitOfWork, user, "Electricity and Gas", true, essentialsCategory);
-            var waterCategory = InsertCategory(unitOfWork, user, "Water", true, essentialsCategory);
-            var internetCategory = InsertCategory(unitOfWork, user, "Internet", true, essentialsCategory);
+            var electricityAndGasCategory = InsertCategory(unitOfWork, user, "Electricity and Gas", true);
+            InsertMonthlyTransactionsForCategory(unitOfWork, primaryChecking, electricityAndGasCategory, 4, "Electricity and Gas", "Electricity and Gas Company", -120, -120);
+
+            var waterCategory = InsertCategory(unitOfWork, user, "Water", true);
+            InsertMonthlyTransactionsForCategory(unitOfWork, primaryChecking, waterCategory, 8, "Water", "Water Company", -30, -30);
+
+            var internetCategory = InsertCategory(unitOfWork, user, "Internet", true);
+            InsertMonthlyTransactionsForCategory(unitOfWork, primaryChecking, internetCategory, 25, "Internet", "Internet Company", -60, -60);
 
             var clothesCategory = InsertCategory(unitOfWork, user, "Clothes", false);
+            InsertRandomMonthlyTransactionsForCategory(unitOfWork, primaryChecking, clothesCategory, "Clothes", "Clothes store", -200, -50);
+
             var electronicsCategory = InsertCategory(unitOfWork, user, "Electronics", false);
+            InsertRandomMonthlyTransactionsForCategory(unitOfWork, primaryChecking, electronicsCategory, "Electronics", "Electronics store", -100, -50);
 
             var hobbyCategory = InsertCategory(unitOfWork, user, "Hobby", false);
             var gamesCategory = InsertCategory(unitOfWork, user, "Games", false, hobbyCategory);
+            InsertRandomMonthlyTransactionsForCategory(unitOfWork, primaryChecking, gamesCategory, "Games", "Games store", -50, -10);
+
             var knittingCategory = InsertCategory(unitOfWork, user, "Knitting", false, hobbyCategory);
+            InsertRandomMonthlyTransactionsForCategory(unitOfWork, primaryChecking, knittingCategory, "Knitting", "Knitting store", -60, -10);
 
             var subscriptionsCategory = InsertCategory(unitOfWork, user, "Subscriptions", true);
             var netflixCategory = InsertCategory(unitOfWork, user, "Netflix", true, subscriptionsCategory);
+            InsertMonthlyTransactionsForCategory(unitOfWork, primaryChecking, netflixCategory, 25, "Netflix", "Netflix subscription", -8, -8);
 
+            // TODO: This should be a standard category, already created issue
             var internalCashflowCategory = InsertCategory(unitOfWork, user, "InternalCashFlow", false);
+            InsertMonthlySavingTransaction(unitOfWork, primaryChecking, savingsAccount, internalCashflowCategory, 26, 100);
         }
 
         private Category InsertCategory(IUnitOfWork unitOfWork, SinanceUser demoUser, string categoryName, bool isRegular, Category parentCategory = null)
@@ -145,6 +164,67 @@ namespace Sinance.Business.DataSeeding
             return category;
         }
 
+        private void InsertMonthlySavingTransaction(IUnitOfWork unitOfWork, BankAccount primaryChecking, BankAccount savingsAccount,
+            Category internalCashflowCategory, int dayOfMonth, int amount)
+        {
+            var today = DateTime.Now.Date;
+
+            DateTime startDate = new DateTime(today.Year, today.Month, dayOfMonth);
+            // Make sure its a historical transaction
+            if (today.Day < dayOfMonth)
+            {
+                startDate = startDate.AddMonths(-1);
+            }
+
+            var transactions = new List<Transaction>();
+            // Insert 2 years of monthly transactions
+            for (var i = 0; i < 24; i++)
+            {
+                transactions.Add(new Transaction
+                {
+                    AccountNumber = "NL02ABNA9450889198",
+                    Amount = -amount,
+                    AmountIsNegative = true,
+                    BankAccount = primaryChecking,
+                    Description = "Savings deposit",
+                    Date = startDate.AddMonths(-i),
+                    TransactionCategories = new List<TransactionCategory>
+                    {
+                        new TransactionCategory
+                        {
+                            Amount = amount,
+                            Category = internalCashflowCategory
+                        }
+                    },
+                    DestinationAccount = "NL83RABO2338418883",
+                    Name = "Savings",
+                    User = primaryChecking.User
+                });
+
+                transactions.Add(new Transaction
+                {
+                    AccountNumber = "NL83RABO2338418883",
+                    Amount = amount,
+                    AmountIsNegative = false,
+                    BankAccount = savingsAccount,
+                    Description = "Savings deposit",
+                    Date = startDate.AddMonths(-i),
+                    TransactionCategories = new List<TransactionCategory>
+                    {
+                        new TransactionCategory
+                        {
+                            Amount = amount,
+                            Category = internalCashflowCategory
+                        }
+                    },
+                    DestinationAccount = "",
+                    Name = "Savings",
+                    User = savingsAccount.User
+                });
+            }
+            unitOfWork.TransactionRepository.InsertRange(transactions);
+        }
+
         private List<Transaction> InsertMonthlyTransactionsForCategory(IUnitOfWork unitOfWork, BankAccount bankAccount, Category category, int dayInMonth,
                     string transactionName, string transactionDescription, int amountMinValue, int amountMaxValue)
         {
@@ -160,7 +240,7 @@ namespace Sinance.Business.DataSeeding
             var transactions = new List<Transaction>();
 
             // Insert 2 years of monthly transactions
-            for (var i = 0; i < 12; i++)
+            for (var i = 0; i < 24; i++)
             {
                 var amount = (decimal)_random.Next(amountMinValue * 100, amountMaxValue * 100) / 100;
 
@@ -185,6 +265,58 @@ namespace Sinance.Business.DataSeeding
                     User = bankAccount.User
                 });
             }
+            unitOfWork.TransactionRepository.InsertRange(transactions);
+
+            return transactions;
+        }
+
+        private List<Transaction> InsertRandomMonthlyTransactionsForCategory(IUnitOfWork unitOfWork, BankAccount bankAccount, Category category, string transactionName, string transactionDescription, int amountMinValue, int amountMaxValue)
+        {
+            var today = DateTime.Now.Date;
+
+            var dayInMonth = _random.Next(1, 25);
+
+            DateTime transactionDate = new DateTime(today.Year, today.Month, dayInMonth);
+            // Make sure its a historical transaction
+            if (today.Day < dayInMonth)
+            {
+                transactionDate = transactionDate.AddMonths(-1);
+            }
+
+            var transactions = new List<Transaction>();
+
+            // Insert 2 years of monthly transactions
+            for (var i = 0; i < 24; i++)
+            {
+                var amount = (decimal)_random.Next(amountMinValue * 100, amountMaxValue * 100) / 100;
+
+                dayInMonth = _random.Next(-7, 7);
+                transactionDate = transactionDate.AddDays(dayInMonth);
+
+                transactions.Add(new Transaction
+                {
+                    AccountNumber = "NL02ABNA9450889198",
+                    Amount = amount,
+                    AmountIsNegative = true,
+                    BankAccount = bankAccount,
+                    Description = transactionDescription,
+                    Date = transactionDate,
+                    TransactionCategories = new List<TransactionCategory>
+                    {
+                        new TransactionCategory
+                        {
+                            Amount = amount,
+                            Category = category
+                        }
+                    },
+                    DestinationAccount = "NL83RABO2338418883",
+                    Name = transactionName,
+                    User = bankAccount.User
+                });
+
+                transactionDate = transactionDate.AddMonths(-1);
+            }
+
             unitOfWork.TransactionRepository.InsertRange(transactions);
 
             return transactions;

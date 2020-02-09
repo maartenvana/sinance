24571f9548e8db111f1224f4d44fd5ad129184e8
Maarten van Arem <maartenvana@users.noreply.github.com>
2020-02-09 15:57:30 +0100

    Code improvements

---
+++ Sinance.Web/wwwroot/scripts/charts/charts-balance-history.js
@@ -0,0 +1,55 @@
+﻿function CreateBalanceHistoryGraph(elementId, data) {
+    $(elementId).highcharts({
+        chart: {
+            zoomType: "x"
+        },
+        title: {
+            text: ""
+        },
+        xAxis: {
+            type: "datetime"
+        },
+        yAxis: {
+            title: {
+                text: ""
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            formatter: ChartCurrencyFormatter
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get("rgba")]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+        series: [
+            {
+                type: "area",
+                name: "euro",
+                data: data
+            }
+        ]
+    });
+}
---
+++ Sinance.Web/wwwroot/scripts/charts/charts-profit-per-year.js
@@ -0,0 +1,39 @@
+﻿function CreateProfitPerYearGraph(elementId, data) {
+    $(elementId).highcharts({
+        chart: {
+            type: "column",
+            zoomType: "x"
+        },
+        title: {
+            floating: true,
+            text: ""
+        },
+        yAxis: {
+            title: {
+                text: ""
+            }
+        },
+        xAxis: {
+            categories: ["Jan", "Feb", "Maa", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"]
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            column: {
+                depth: 25
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            formatter: ChartCurrencyFormatter
+        },
+        series: [
+            {
+                data: data
+            }
+        ]
+    });
+}

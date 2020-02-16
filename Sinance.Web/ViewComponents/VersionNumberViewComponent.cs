55eb602e4b2b00798204327c2327b609cd510e13
Maarten van Arem <maartenvana@users.noreply.github.com>
2020-02-16 13:21:08 +0100

    Added version number

---
+++ Sinance.Web/ViewComponents/VersionNumberViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sinance.Common.Configuration;
+
+namespace Sinance.Web.ViewComponents
+{
+    public class VersionNumberViewComponent : ViewComponent
+    {
+        private readonly AppSettings _appSettings;
+
+        public VersionNumberViewComponent(AppSettings appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_appSettings.SinanceVersion);
+        }
+    }
+}
